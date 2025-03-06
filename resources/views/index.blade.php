b9d42bb6829f795692f00d6e6ed098f2639fe3b7
HaoVo2007 <hao01638081724@gmail.com>
2025-03-06 15:38:25 +0700

    test railway

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 
 @section('content')
+<h1>vasfl;safsdvnlcxsdlvsad;lf</h1>
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
